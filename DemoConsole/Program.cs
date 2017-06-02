27e839f99ced80d96065defd2c33004d27dec3ed
cutelilpuppy <zbqs@hasanmail.ml>
2017-06-02 14:25:43 -0500

    Remove Useless Shit

--- DemoConsole/Program.cs
+++ DemoConsole/Program.cs
@@ -9,10 +9,7 @@ namespace YoutubeExplode.DemoConsole
 {
     public static class Program
     {
-        /// <summary>
-        /// If given a youtube url, parses video id from it.
-        /// Otherwise returns the same string.
-        /// </summary>
+        
         private static string NormalizeId(string input)
         {
             if (!YoutubeClient.TryParseVideoId(input, out string id))
@@ -20,59 +17,29 @@ namespace YoutubeExplode.DemoConsole
             return id;
         }
 
-        /// <summary>
-        /// Turns file size in bytes into human-readable string
-        /// </summary>
-        private static string NormalizeFileSize(long fileSize)
-        {
-            string[] units = { "B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB" };
-            double size = fileSize;
-            int unit = 0;
-
-            while (size >= 1024)
-            {
-                size /= 1024;
-                ++unit;
-            }
-
-            return $"{size:0.#} {units[unit]}";
-        }
-
         private static async Task MainAsync()
         {
-            // Client
             var client = new YoutubeClient();
-
-            // Get the video ID
-            Console.Write("Enter Youtube video ID or URL: ");
             string id = Console.ReadLine();
             id = NormalizeId(id);
 
-            // Get the video info
-            Console.WriteLine("Loading...");
             var videoInfo = await client.GetVideoInfoAsync(id);
             Console.WriteLine('-'.Repeat(100));
 
-            // Print metadata
             Console.WriteLine($"Id: {videoInfo.Id} | Title: {videoInfo.Title} | Author: {videoInfo.Author.Title}");
 
-            // Get the most preferable stream
-            Console.WriteLine("Looking for the best mixed stream...");
             var streamInfo = videoInfo.MixedStreams
                 .OrderBy(s => s.VideoQuality)
                 .Last();
             string normalizedFileSize = NormalizeFileSize(streamInfo.ContentLength);
             Console.WriteLine($"Quality: {streamInfo.VideoQualityLabel} | Container: {streamInfo.Container} | Size: {normalizedFileSize}");
 
-            // Compose file name, based on metadata
             string fileExtension = streamInfo.Container.GetFileExtension();
             string fileName = $"{videoInfo.Title}.{fileExtension}";
 
-            // Remove illegal characters from file name
             fileName = fileName.Except(Path.GetInvalidFileNameChars());
 
-            // Download video
-            Console.WriteLine($"Downloading to [{fileName}]...");
+            Console.WriteLine($"Downloading [{fileName}]...");
             Console.WriteLine('-'.Repeat(100));
 
             var progress = new Progress<double>(p => Console.Title = $"YoutubeExplode Demo [{p:P0}]");
@@ -84,13 +51,6 @@ namespace YoutubeExplode.DemoConsole
 
         public static void Main(string[] args)
         {
-            // This demo prompts for video ID, gets video info and downloads one media stream
-            // It's intended to be very simple and straight to the point
-            // For a more complicated example - check out the WPF demo
-
-            Console.Title = "YoutubeExplode Demo";
-
-            // Main method in consoles cannot be asynchronous so we run everything synchronously
             MainAsync().GetAwaiter().GetResult();
         }
     }
